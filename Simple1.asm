727044af584b154179da9be6dd78197f910ceac4
jgd16 <jgd16@skbl-422-015.ph.ic.ac.uk>
2019-01-25 09:23:33 +0000

    Changed output port to PORT C

--- Simple1.asm
+++ Simple1.asm
@@ -7,15 +7,60 @@
 	org 0x100		    ; Main code starts here at address 0x100
 
 start
-	movlw 	0x0
-	movwf	TRISC, ACCESS	    ; Port C all outputs
-	bra 	test
-loop	movff 	0x06, PORTC
-	incf 	0x06, W, ACCESS
-test	movwf	0x06, ACCESS	    ; Test for end of loop condition
-	movlw 	0x63
-	cpfsgt 	0x06, ACCESS
-	bra 	loop		    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
 	
+	movlw 0x00 ;make E,F,H tristates
+	movwf TRISE, ACCESS
+	movlw 0x00
+	movwf TRISF, ACCESS
+	movlw 0x00
+	movwf TRISH, ACCESS
+	movlw 0x00
+	movwf TRISJ, ACCESS
+	
+	movlw 0x02 ;OE 1
+	movwf PORTF, ACCESS
+	
+	movlw 0xAA ;data bus
+	movwf PORTE, ACCESS
+	nop
+	nop
+	nop
+	nop
+	
+	movlw 0x06 ;clock and CP 1
+	movwf PORTF, ACCESS
+	nop
+	nop
+	nop
+	movlw 0xFF
+	movwf TRISE, ACCESS
+	
+	
+	movff PORTE, PORTH
+
+	
+	movlw 0x8 ;OE2 to 1
+	movwf PORTF, ACCESS
+	
+	movlw 0xAB ;data bus
+	movwf PORTE, ACCESS
+	nop
+	nop
+	nop
+	nop
+	
+	movlw 0x18 ;clock2 and CP2 1
+	movwf PORTF, ACCESS
+	nop
+	nop
+	nop
+	movlw 0xFF
+	movwf TRISE, ACCESS
+	movlw 0x00
+	movwf PORTF, ACCESS
+	
+	
+	movff PORTE, PORTJ
+	
+loop	goto loop
 	end
