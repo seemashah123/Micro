90f1ce822db1b6f1e4a07d8472784e20641724c7
jgd16 <jgd16@skbl-422-015.ph.ic.ac.uk>
2019-02-21 14:38:14 +0000

    - PORT B pin 5 as input - gets random number

--- wordsDatabase.asm
+++ wordsDatabase.asm
@@ -6,13 +6,13 @@
     extern	counter2, counter	
 
 
-int_hi	code	0x0008 ; high vector, no low vector ;joe
-    btfss   INTCON,RBIE ; check that this is RBIF interrupt
-    retfie  FAST ; if not then return
-    ;incf    LATD ; increment PORTD
-    ;add code here to add one to counter and double it
-    bcf	    INTCON,RBIF ; clear interrupt flag
-    retfie  FAST ; fast return from interrupt
+;int_hi	code	0x0008 ; high vector, no low vector ;joe
+;    btfss   INTCON,RBIE ; check that this is RBIF interrupt
+;    retfie  FAST ; if not then return
+;    ;incf    LATD ; increment PORTD
+;    ;add code here to add one to counter and double it
+;    bcf	    INTCON,RBIF ; clear interrupt flag
+;    retfie  FAST ; fast return from interrupt
 
 
     
